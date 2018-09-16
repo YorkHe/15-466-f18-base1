a604107a5d7a067d665ec8da89bdf021dd285da0
YorkHe <kiddmagician@gmail.com>
2018-09-16 16:09:44 -0400

    add sounds

---
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/CratesMode.cpp.o"
+  "CMakeFiles/main.dir/GameMode.cpp.o"
+  "CMakeFiles/main.dir/Load.cpp.o"
+  "CMakeFiles/main.dir/MenuMode.cpp.o"
+  "CMakeFiles/main.dir/MeshBuffer.cpp.o"
+  "CMakeFiles/main.dir/Mode.cpp.o"
+  "CMakeFiles/main.dir/PhoneMode.cpp.o"
+  "CMakeFiles/main.dir/Scene.cpp.o"
+  "CMakeFiles/main.dir/WalkMesh.cpp.o"
+  "CMakeFiles/main.dir/compile_program.cpp.o"
+  "CMakeFiles/main.dir/data_path.cpp.o"
+  "CMakeFiles/main.dir/draw_text.cpp.o"
+  "CMakeFiles/main.dir/gl_shims.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/vertex_color_program.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
